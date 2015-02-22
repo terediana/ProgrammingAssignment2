05735ec3505fc9b564c6a7b252f19487d909fd50
terediana <tere40@gmail.com>
2015-02-22 00:15:31 -0600

    More Changes in cachamatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
 ## The function makeCacheMatrix receives the matrix,
 ## the default value is an empty matrix.
-## Function creates a list of 4 functions for operating on a matrix:
-## 1. a function to set the values of the elements of the matrix
+## Function creates a list of 4 functions for operating on the matrix:
+## 1. a function to set the values of the elements of the matrix 
 ## 2. a function to get the matrix
 ## 3. a function to set the inverse of the matrix
 ## 4. a function to get the inverse of the matrix
@@ -15,49 +15,49 @@ makeCacheMatrix <- function(x = matrix()) {
   
   inversa <<- NULL 
   
-  set <- function(y) { 
-    x <<- y 
+  set <- function(y) { ## function to set the value of matrix and
+    x <<- y            ## to initialize the inverse with NULL.  
     inversa <<- NULL 
     
   }
   
-  get <- function() x
+  get <- function() x ## función to get the  matrix.
   
-  setinv <- function(inV) inversa <<- inV
+  setinv <- function(inV) inversa <- inV  ## function to set the the inverse matrix.
   
-  getinv <- function() inversa
+  getinv <- function() inversa ## function to get the the inverse matrix.
   
-  list(set = set, get = get,
+  ## the object is created. It is a list of 4 functions.
+  list(set = set, get = get, 
        setinv = setinv,
        getinv = getinv)
   
 }
 
 
-## Write a short comment describing this function
-## The function receives a list of function for
+
+## The function receives a list of functions for
 ## operating on a matrix created by makeCacheMatrix 
 ## Function calculates the inverse of the matrix only
-## if there is not computed before. If the inverse exists
-## function does not compute the inverse
+## if it is not computed before. 
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
-  inversa <- x$getinv()
+  inversa <- x$getinv() ## get the inverse
   
-  
+  ## if the inverse was calculated
   if( !is.null(inversa ) ) { 
-    message("getting cached inverse matrix")
-    return(inversa)
+    message("getting cached inverse matrix") ## take it  
+    return(inversa) ## take it. 
     
   }
   
   
-  inversa <- solve(data)
+  inversa <- solve(x$get()) ## if the inverse was not calculated, it is need no find
+                         ## using solve function.
+  x$setinv(inversa) ## set the value using the object function setinv
   
-  x$setinv(inversa)
-  
-  inversa
+  inversa ## take it.
   
 }
